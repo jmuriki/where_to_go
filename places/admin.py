b5f150a341017089078ef74691a37be4f891f1d1
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-01-30 22:15:41 +0530

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from places.models import Place
+from places.models import Place, Image
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-	list_display = [
-		'title',
-	]
+    list_display = [
+        'title',
+    ]
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+        list_display = [
+        'image',
+    ]
--- places/models.py
+++ places/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
 class Place(models.Model):
-	title = models.CharField('Название', max_length=200)
-	description_short = models.TextField('Короткое описание')
-	description_long = models.TextField('Длинное описание')
-	coordinates_lng = models.FloatField('Долгота', blank=True)
-	coordinates_lat = models.FloatField('Широта', blank=True)
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Короткое описание')
+    description_long = models.TextField('Длинное описание')
+    coordinates_lng = models.FloatField('Долгота', blank=True)
+    coordinates_lat = models.FloatField('Широта', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField('Изображение', null=True, blank=True)
+    place = models.ForeignKey(Place, related_name="images", on_delete=models.CASCADE)
+
+
