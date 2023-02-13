9ea2ac65105eb84791c037ec66169250a888c697
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-02-13 15:00:20 +0530

    Add places.urls to use app-level routing

---
+++ places/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from places import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('places/<int:place_id>', views.get_place_json, name="place_json"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from where_to_go import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from where_to_go import views
+
 
 urlpatterns = [
+    path('', include('places.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index),
     path('tinymce/', include('tinymce.urls')),
-    path('places/<int:place_id>', views.get_place_json, name="place_json"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
