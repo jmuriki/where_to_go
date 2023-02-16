9223d38ba4269d0addd817973cc69e4c7201bc4b
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-02-16 17:38:23 +0530

    Add "/" at the end of every path

--- places/urls.py
+++ places/urls.py
@@ -7,5 +7,5 @@ from places import views
 
 urlpatterns = [
     path('', views.index),
-    path('places/<int:place_id>', views.get_place_json, name="place_json"),
+    path('places/<int:place_id>/', views.get_place_json, name="place_json"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
